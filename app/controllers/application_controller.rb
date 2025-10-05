beec09709ecf5f0da82d2d2eacf02a78a609ac17
Mark Schiltz <markschiltz@Amys-MacBook-Pro-2.local>
2025-10-05 10:36:12 -0400

    regex and processing working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,30 @@ class ApplicationController < ActionController::Base
 
   def home
     if request.post?
-      @contents = 'parse here'
+      begin
+        files = params[:files]
+        @contents = process_files(files)
+      rescue => e
+        raise 'Something went wrong'
+      end
     end
+  end
+
+  private
+
+  def process_files(files)
+    days = []
+    regex = /Project (?<project_number>\d+): (?<high_low>high|low) Cost City Start Date: (?<start_date>\d{1,2}\/\d{1,2}\/\d{2}) End Date: (?<end_date>\d{1,2}\/\d{1,2}\/\d{2})/i
+    
+    files.each do |file|
+      File.foreach(file.tempfile.path) do |line|
+        match = line.match(regex)
+        if match
+          days.push(match.named_captures) 
+        end
+      end
+    end
+    days
   end
 
 end
