5aa0ebbb1e9c1cdccdbbb7a8e8e763fe972795f7
Mark Schiltz <markschiltz@Amys-MacBook-Pro-2.local>
2025-10-06 20:36:04 -0400

    added text field and a bit of error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,19 @@ class ApplicationController < ActionController::Base
   def home
     if request.post?
       begin
+        @errors = []
+        parsed_data = []
         files = params[:files]
-        parsed_data = process_files(files)
+        text = params[:text]
+
+        if files
+          parsed_data += process_files(files)
+        end
+
+        if text
+          parsed_data += process_text(text)
+        end
+        
         @days = data_to_days(parsed_data)
         mark_traveling(@days)
         calculate(@days)
@@ -16,28 +27,56 @@ class ApplicationController < ActionController::Base
 
   private
 
+  DATE_FORMAT = '%m/%d/%y'
+  REGEX = /Project (?<project_number>\d+): (?<high_low>high|low) Cost City Start Date: (?<start_date>\d{1,2}\/\d{1,2}\/\d{2}) End Date: (?<end_date>\d{1,2}\/\d{1,2}\/\d{2})/i
+
   # process the provided files and use a regex to get the valid data into an array, one entry per line
   def process_files(files)
     projects = []
-    date_format = '%m/%d/%y'
-    regex = /Project (?<project_number>\d+): (?<high_low>high|low) Cost City Start Date: (?<start_date>\d{1,2}\/\d{1,2}\/\d{2}) End Date: (?<end_date>\d{1,2}\/\d{1,2}\/\d{2})/i
     
     files.each do |file|
       File.foreach(file.tempfile.path) do |line|
-        match = line.match(regex)
+        match = line.match(REGEX)
 
         if match
           projects.push({
             project_number: match['project_number'],
             cost:           match['high_low'],
-            start_date:     Date.strptime(match['start_date'], date_format),
-            end_date:       Date.strptime(match['end_date'], date_format)
+            start_date:     Date.strptime(match['start_date'], DATE_FORMAT),
+            end_date:       Date.strptime(match['end_date'], DATE_FORMAT)
           })
+        else
+          @errors.push("There was a problem parsing file: #{file.original_filename}.")
+          return []
         end
 
       end
     end
     projects
+  end
+
+  # process the provided text, same as above
+  def process_text(text)
+    projects = []
+    
+    text.each_line do |line|
+        match = line.match(REGEX)
+
+        if match
+          projects.push({
+            project_number: match['project_number'],
+            cost:           match['high_low'],
+            start_date:     Date.strptime(match['start_date'], DATE_FORMAT),
+            end_date:       Date.strptime(match['end_date'], DATE_FORMAT)
+          })
+        else
+          @errors.push("There was a problem parsing your text.")
+          return []
+        end
+
+    end
+    projects
+
   end
 
   # take the array of data points and create a hash of entries, using the date timestamp as the key
